1655b23d9d95de19d5149c4458a462c530eb76d5
yasuda0102 <webnuac@gmail.com>
2020-05-02 08:12:09 +0900

    乗算回路を書いた。といってもラップするモジュール書いただけ

--- translate.sh
+++ translate.sh
@@ -2,5 +2,6 @@
 
 nsl2vl adder32.nsl
 nsl2vl shifter.nsl
+nsl2vl multiplier32.nsl
 nsl2vl driver.nsl -target driver -verisim2
-iverilog adder32.v shifter.v driver.v
+iverilog adder32.v shifter.v multiplier32.v driver.v
