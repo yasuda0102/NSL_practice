3eb84676600b653dbf82ffb113f7d7853a5e4796
yasuda0102 <webnuac@gmail.com>
2020-05-02 15:20:39 +0900

    命名規則の統一

--- translate.sh
+++ translate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 nsl2vl adder32.nsl
-nsl2vl shifter.nsl
+nsl2vl shifter32.nsl
 nsl2vl multiplier32.nsl
 nsl2vl alu32.nsl
 nsl2vl regfile.nsl
 nsl2vl mem.nsl
 nsl2vl primitive_cpu.nsl
 nsl2vl driver.nsl -target driver -verisim2
-iverilog adder32.v shifter.v multiplier32.v alu32.v regfile.v mem.v primitive_cpu.v driver.v
+iverilog adder32.v shifter32.v multiplier32.v alu32.v regfile.v mem.v primitive_cpu.v driver.v
