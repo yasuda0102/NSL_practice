ff1780cc37ab57b59cb122cd0b0c9e2e924ec9a1
yasuda0102 <webnuac@gmail.com>
2020-05-14 23:22:35 +0900

    translate.shの修正

--- translate.sh
+++ translate.sh
@@ -6,6 +6,6 @@ nsl2vl multiplier32.nsl
 nsl2vl alu32.nsl
 nsl2vl comparator32.nsl
 nsl2vl regfile.nsl
-nsl2vl primitive_cpu.nsl -target primitive_cpu
+nsl2vl primitive_cpu.nsl
 nsl2vl driver.nsl -target driver -verisim2
 iverilog adder32.v shifter32.v multiplier32.v alu32.v comparator32.v regfile.v dpram.v primitive_cpu.v driver.v
