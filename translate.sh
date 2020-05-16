b3f4a2654b2cf38cd57f2059955eaac5a8a696c9
yasuda0102 <webnuac@gmail.com>
2020-05-16 18:09:53 +0900

    synopsys translate_offを無効化するために最適化オプションを追加

--- translate.sh
+++ translate.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-nsl2vl adder32.nsl
-nsl2vl shifter32.nsl
-nsl2vl multiplier32.nsl
-nsl2vl alu32.nsl
-nsl2vl comparator32.nsl
-nsl2vl regfile.nsl
-nsl2vl primitive_cpu.nsl
+nsl2vl -O adder32.nsl
+nsl2vl -O shifter32.nsl
+nsl2vl -O multiplier32.nsl
+nsl2vl -O alu32.nsl
+nsl2vl -O comparator32.nsl
+nsl2vl -O regfile.nsl
+nsl2vl -O primitive_cpu.nsl
 nsl2vl driver.nsl -target driver -verisim2
 iverilog driver.v adder32.v shifter32.v multiplier32.v alu32.v comparator32.v regfile.v dpram.v primitive_cpu.v
